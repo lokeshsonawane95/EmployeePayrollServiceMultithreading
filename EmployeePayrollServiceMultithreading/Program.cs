237c8728e64a5c9885644979c02c0799e80b0d24
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-23 21:07:05 +0530

    Added to main

---
+++ EmployeePayrollServiceMultithreading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollServiceMultithreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
